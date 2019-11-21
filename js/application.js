318b49beaaaf9bb299c07620914a068271c20a50
kwuu <wu.kevin83@gmail.com>
2019-11-21 13:55:11 -0500

    Image rotator (#1)
    * image rotator
    
    * image rotator

--- js/application.js
+++ js/application.js
@@ -6,3 +6,15 @@ $(document).on('click', '.anchor-link', function(e){
 		scrollTop: $(thisHref).offset().top
 	}, 500);
 });
+
+// ROTATOR
+// www.sitepoint.com/make-a-simple-javascript-slideshow-without-jquery 
+var slides = document.querySelectorAll('.heroshot .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
